56eeb27efde14d7ed7e72f019ec7b6d4550e94d1
aworsley9@googlemail.com <1309Adam1309Adam>
2020-07-03 18:14:21 +0100

    fixed formatting

--- upload.py
+++ upload.py
@@ -6,26 +6,28 @@ import json
 
 url = 'https://anonfile.com/api/upload'
 
-if len(sys.argv) == 1: # If no files are specified print the error and exit.
+if len(sys.argv) == 1:  # If no files are specified print the error and exit.
     print("[ERROR] You need to specify one or more files!")
 
-for filename in sys.argv[1:]: # For every file specified.
+for filename in sys.argv[1:]:  # For every file specified.
     try:
-        files = {'file': (open(filename, 'rb'))} # Opens the file and creates a dictionary.
-    except FileNotFoundError: # If the file doesn't exist.
+        # Opens the file and creates a dictionary.
+        files = {'file': (open(filename, 'rb'))}
+    except FileNotFoundError:  # If the file doesn't exist.
         print(f'[ERROR] The file "{filename}" doesn\'t exist!')
-        continue # Continues the loop for any valid files.
-    except IsADirectoryError: # If the file is a directory and not a file.
+        continue  # Continues the loop for any valid files.
+    except IsADirectoryError:  # If the file is a directory and not a file.
         print('[ERROR] You cannot upload a directory!')
-        continue # Continues the loop for any valid files.
-    r = requests.post(url, files=files) # HTTP Request to the API.
+        continue  # Continues the loop for any valid files.
+    r = requests.post(url, files=files)  # HTTP Request to the API.
     print("[UPLOADING]", filename)
     request_dict = json.loads(r.text)
     if request_dict['status']:
         urlshort = request_dict['data']['file']['url']['short']
         urllong = request_dict['data']['file']['url']['full']
-        print(f'[SUCCESS] Your file has been succesfully uploaded:\nFull URL: {urllong}\nShort URL: {urlshort}')
-    else: # If the GET request returns an error
+        print(
+            f'[SUCCESS] Your file has been succesfully uploaded:\nFull URL: {urllong}\nShort URL: {urlshort}')
+    else:  # If the GET request returns an error
         message = request_dict['error']['message']
         errtype = request_dict['error']['type']
-        print(f'[ERROR] {message}\n{errtype}') # Prints the error
+        print(f'[ERROR] {message}\n{errtype}')  # Prints the error
